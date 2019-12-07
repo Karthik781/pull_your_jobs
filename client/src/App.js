e161bbbec3fd6480e3fd5853e36f7b401792b488
Karthik Kedam <karthikkedam@gmail.com>
2019-12-07 23:55:58 +0530

    add workers

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Jobs from './components/jobs.component'
 
@@ -6,6 +6,10 @@ const mockData = [
   {title: "se 1", company: "google"},
   {title: "se 2", company: "Amazon"}
 ]
+
+const GITHUB_API_URL = "http://localhost:3001/jobs";
+
+const [jobList, updatedJobList] = useState([]);
 
 function App() {
   return (
